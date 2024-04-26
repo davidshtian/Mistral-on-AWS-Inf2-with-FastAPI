53597343db7621beb56b05c931c3956c4978d039
davidshtian <david881223@outlook.com>
2024-04-26 16:33:01 +0800

    Update embeds_client.py

--- client/embeds_client.py
+++ client/embeds_client.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 
 image_embeds_saved = np.load('input_embeds.npy')
-payload = {'inputs': image_embeds_saved.tolist()}
+payload = {'inputs': image_embeds_saved.tolist(), 'parameters': {'max_new_tokens': 192}}
 
 url = 'http://127.0.0.1:8000/generate'
 headers = {'Content-Type': 'application/json'}
